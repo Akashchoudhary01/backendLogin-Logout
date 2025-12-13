b6caf540cd04ce10b8bb551e1dfbd32527585b6c
Akash <akashkrchoudhary007@gmail.com>
2025-12-13 22:45:19 +0530

    mongodb

---
+++ Config/databaseConfig.js
@@ -0,0 +1,9 @@
+import mongoose from ('mongoose');
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/demooo";
+
+export const databaseConect = () =>{
+    mongoose.connect(MONGODB_URL)
+    .then((conn)=> console.log(`connect to db ${conn.connection.host}`))
+    .catch((err)=> console.log(err.message));
+}
