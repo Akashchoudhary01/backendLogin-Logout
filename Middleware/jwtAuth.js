85041696503f05eb19255f9f2557b9b5fea73ad4
Akash <akashkrchoudhary007@gmail.com>
2026-01-17 22:39:50 +0530

    ..

--- Middleware/jwtAuth.js
+++ Middleware/jwtAuth.js
@@ -1,19 +1,25 @@
-import  JWT   from "jsonwebtoken";
+import JWT from "jsonwebtoken";
 
-export const jwtAuth = (req , res , next)=>{
+export const jwtAuth = (req, res, next) => {
+  const token = (req.cookies && req.cookies.token) || null;
+
+  if (!token) {
+    return res.status(400).json({
+      success: false,
+      message: "Not Authorized",
+    });
+  }
+
+  try {
+    const Payload = JWT.verify(token , process.env.SECRET_KEY);
+    req.user = {id : Payload.id , email : Payload.email}
     
-        const token = (req.cookies && req.cookies.token) || null;
-        
-     if(!token){
-         return res.status(400).json({
-             success: false,
-             message : 'Not Authorized'
-            })
-        }
-        
-    
+  } catch (e) {
+    return res.status(400).json({
+           success : false,
+           message : 'Not Authorized'
+    })    
+  }
 
-
-    next();
-
-}
+  next();
+};
