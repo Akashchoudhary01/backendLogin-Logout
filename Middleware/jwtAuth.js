9edcee38d5b3c75a63823cdb377a3387a6e950a6
Akash <akashkrchoudhary007@gmail.com>
2026-01-17 22:30:03 +0530

    .

--- Middleware/jwtAuth.js
+++ Middleware/jwtAuth.js
@@ -1,16 +1,17 @@
 import  JWT   from "jsonwebtoken";
 
 export const jwtAuth = (req , res , next)=>{
-    try {
+    
         const token = (req.cookies && req.cookies.token) || null;
         
-    } catch (e) {
-        return res.status(400).json({
-            success: false,
-            message : 'user does not exists'
-        })
+     if(!token){
+         return res.status(400).json({
+             success: false,
+             message : 'Not Authorized'
+            })
+        }
         
-    }
+    
 
 
     next();
